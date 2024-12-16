217aa62b843237baf76f86c57d1adcd5dcbdf97b
Morgana <morganaschnneider@gmail.com>
2024-12-16 16:10:27 -0300

    Add files via upload

---
+++ cursojs/aula4/comentarios.js
@@ -0,0 +1,13 @@
+//  comentário aqui
+console.log('Olá mundo!');
+console.log('Linha 1');
+
+/* um bloco de código
+Linha de comentário.
+*/
+
+console.log('Linha 2')
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+// Vai ser ignorado
