ca51578b2b5d7c6181e38a955c1e5e14fe6c3265
potterred <morganaschnneider@gmail.com>
2025-01-14 01:02:25 -0300

    js

--- cursojs/aula11/script.js
+++ cursojs/aula11/script.js
@@ -1,2 +1,8 @@
-const num1 = prompt('Digite um numero');
-const num2 = prompt('Digite um numero');
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+alert(`O resultado da sua conta foi: ${num1 +num2}`);
