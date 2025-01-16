a2d857700518263293a9078b5fe9c0d76d814ae0
potterred <morganaschnneider@gmail.com>
2025-01-16 20:42:17 -0300

    js

--- cursojs/aula13/aula13.js
+++ cursojs/aula13/aula13.js
@@ -1,3 +1,7 @@
 // 0123456789
 
-let umaString = "o rato roeu a roupa do rei de roma"
+let umaString = "o rato roeu a roupa do rei de roma";
+
+console.log(umaString.toLocaleUpperCase);
+console.log(umaString.toLocaleLowerCase);
+
