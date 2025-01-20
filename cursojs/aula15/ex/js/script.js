8e7fdad46dc1dd5485f8d3b7f2a6c79e2a720130
potterred <morganaschnneider@gmail.com>
2025-01-20 17:42:48 -0300

    js

--- cursojs/aula15/ex/js/script.js
+++ cursojs/aula15/ex/js/script.js
@@ -3,3 +3,10 @@ Raiz quadrada: 7.83912983
 55.1987555 é inteiro: false
 É Nan: false
 Arredondando para baixo:55
+Arredondando para cima :56
+Com duas casas decimais: 55.20
+*/
+
+numeroTitulo.innerHTL = numero;
+texto.innerHTL = `<p>Raiz quadrada: ${numero ** 0.5}</p>`
+texto.innerHTL = `<p>${numero} é inteiro: ${Number.isInteger}`
