dda3888151410cb923d3a8b115691e5961729c35
potterred <morganaschnneider@gmail.com>
2025-02-10 19:34:04 -0300

    js

---
+++ cursojs/aula18/index.js
@@ -0,0 +1,9 @@
+function criarPessoa (nome, sobrenome, idade) {
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade,
+};
+}
+
+const pessoal = criarPessoa('Morgana', 'Schneider', 29);
