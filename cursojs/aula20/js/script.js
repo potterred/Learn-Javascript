ea54fd7f59770eb1ceb1c51d1a1705771a534fba
potterred <morganaschnneider@gmail.com>
2025-03-14 10:37:52 -0300

    js

--- cursojs/aula20/js/script.js
+++ cursojs/aula20/js/script.js
@@ -1,10 +1,21 @@
 function meuEscopo (){
 const form = document.querySelector('.form');
-// form.onsubmit = function (evento){
-// evento.preventDefault();
-// alert(1);
-// console.log('Foi enviado');
-//};
-form.addEventListener('submit');
+const resultado = document.querySelector('.resultado');
+
+const pessoas = [];
+
+function recebeEventoForm (evento) {
+    evento.preventDefault();
+
+    const nome = form.querySelector('.nome');
+    const sobrenome = form.querySelector('.sobrenome');
+    const peso = form.querySelector('.peso');
+    const altura = form.querySelector('.altura');
+
+    console.log(nome.value, sobrenome, peso, altura);
+
+}
+
+form.addEventListener('submit', recebeEventoForm);
 }
 meuEscopo();
