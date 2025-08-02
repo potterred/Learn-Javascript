806617e7edb0972f42d0648f994d9ad55a93f120
unknown <morganaschnneider@gmail.com>
2025-08-02 05:25:34 -0300

    js

--- JS/aula25/index.js
+++ JS/aula25/index.js
@@ -1,11 +1,20 @@
-const numero = 22;
+const numero = 8;
+
+//  Se (numero >= 0 && numero <= 5) ocorrer, faça isso {código}
+//  Se não faça isso {o código}
+
+if (numero < 10) {
+    console.log('O numero é menor que 10.')
+}
 
 if (numero >= 0 && numero <= 5){
     console.log('O número está entre 0 e 5');
 }else if (numero >= 6 && numero <= 8){
     console.log('O número está entre 6 e 8');
 }else if (numero >= 9 && numero <= 11){
-    console.log('O número está entre 9 e 11')
+    console.log('O número está entre 9 e 11') // Verdadeiro
 }else{
     console.log('O número não está entre 0 e 11')
-}   
+}
+
+console.log('...Aqui vai o resto do código');
